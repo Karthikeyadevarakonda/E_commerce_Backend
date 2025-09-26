dd32da869bc3af72a65af65a99a820a6f83cd22b
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-09-26 19:32:02 +0530

    enabled cors to allow frontend to acess

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ import metricRoutes from "./routes/metrics/route.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/api/allProducts", productRoutes);
@@ -17,10 +23,6 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/admin", metricRoutes);
 
 const PORT = process.env.PORT || 7002;
-
-app.get("/", (_, res) => {
-  res.send("HELLO WORLD");
-});
 
 app.listen(PORT, () => {
   console.log(`YOUR APP RUNNING ON PORT : http://localhost:${PORT}`);

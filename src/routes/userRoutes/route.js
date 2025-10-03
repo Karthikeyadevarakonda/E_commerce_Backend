8f5a4922d28d36dd94672de79730013c35fc8d66
KarthikeyaDevarakonda <karthikeya8787@gmail.com>
2025-10-03 14:17:05 +0530

    deploy

--- src/routes/userRoutes/route.js
+++ src/routes/userRoutes/route.js
@@ -10,8 +10,8 @@ import { isAuthenticated } from "../../middlewares/authCheck.js";
 
 const router = express.Router();
 
-router.get("/", isAuthenticated, getAllProducts);
-router.get("/:id", isAuthenticated, getProductById);
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
 router.post("/", isAuthenticated, addProduct);
 router.put("/:id", isAuthenticated, updateProductById);
 router.delete("/:id", isAuthenticated, deleteProductById);
